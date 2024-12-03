1c617473cbad2ae97a8dd4604481294792c03ee5
toddb <toddb@10.247.206.30>
2024-12-03 16:46:22 -0600

    Initial commit

---
+++ src/main/java/csci1011/githubstuff/GitHubStuff.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package csci1011.githubstuff;
+
+/**
+ *
+ * @author toddb
+ */
+public class GitHubStuff {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
